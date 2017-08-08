59dde7ef2af913ba8a26ba584a1a0cd50335228f
mikesmall <michael.l.a.small@gmail.com>
2017-08-08 17:32:39 -0400

    Exercise 5 all done

--- closure_exercises/exercise5.js
+++ closure_exercises/exercise5.js
@@ -1,11 +1,13 @@
-
 // Generate another function that requires arguments.
 // This time you'll have to set up the flyTo variable yourself.
 
 var personToVisit = "best friend";
 // YOUR CODE HERE ------------------------
 
-
+function flyTo(destination) {
+  var personToVisit = "Applesauce McGee";
+  console.log("Enjoy your trip to " + destination + " with " + personToVisit + "!!!");
+};
 
 // ---------------------------------------
 
@@ -14,3 +16,5 @@ flyTo("Shanghai");
 personToVisit = "Toronto"; // (has no effect on next method call)
 flyTo("Lagos");
 // Enjoy your visit to Lagos to visit your best friend.
+
+// I changed it to the name of my real best friend, Applesauce McGee.
