3ac6e46bd2310c21071524a6dfbafb00d6761586
mikesmall <michael.l.a.small@gmail.com>
2017-08-08 16:52:49 -0400

    Exercise 1 done

--- closure_exercises/exercise1.js
+++ closure_exercises/exercise1.js
@@ -5,9 +5,20 @@ var printNumberFive = getPrintNumberFunction(5);
 
 // YOUR CODE HERE ------------------------
 
-
+function getPrintNumberFunction(theNumber) {
+  return function() {
+    console.log(theNumber);
+  }
+};
 
 // ---------------------------------------
 
 printNumberThree(); // 3
 printNumberFive(); // 5
+
+// Mike speaking:
+// Weird thing about this is, I can just do this:
+// > var printNumberFour = 4
+// > printNumberFour
+// ... and it still outputs...
+// 4 
