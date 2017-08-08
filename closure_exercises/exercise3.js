9e7e97c21797666b4cfe5370ba35bd332a8f6b66
mikesmall <michael.l.a.small@gmail.com>
2017-08-08 17:12:54 -0400

    Exercise 3 done

--- closure_exercises/exercise3.js
+++ closure_exercises/exercise3.js
@@ -2,12 +2,16 @@
 // Multiple arguments!
 
 var sandwiches = 2,
-    burgers = 3;
+    burgers    = 3;
 
-var theUsual = getUsualOrderFunction(2, 3);
+function theUsual () {
+  return getUsualOrderFunction(2, 3);
+}
 // YOUR CODE HERE ------------------------
 
-
+function getUsualOrderFunction(sandwiches, burgers) {
+  console.log("We'll get " + sandwiches + " sandwiches and " + burgers + " burgers.")
+};
 
 // ---------------------------------------
 
@@ -15,3 +19,8 @@ theUsual(); // We'll get 2 sandwiches and 3 burgers.
 sandwiches = 0;
 burgers = 1;
 theUsual(); // We'll get 2 sandwiches and 3 burgers.
+
+// Observation:
+// theUsual(); is impervious to change (always 2 and 3), but if I enter 
+// getUsualOrderFunction(6,7);
+// Node returns "We'll get 6 sandwiches and 7 burgers."
