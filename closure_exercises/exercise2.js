623e3757b99c18b0dd0ff42fd6b5f54f71222070
mikesmall <michael.l.a.small@gmail.com>
2017-08-08 17:02:05 -0400

    Exercise 2 complete

--- closure_exercises/exercise2.js
+++ closure_exercises/exercise2.js
@@ -2,14 +2,22 @@
 // This example proves that the data in the closure is protected from the outside.
 // You'll have to set up a variable called tellFavouriteMovie to be a function that remembers the value "The Matrix".
 // If you're stuck, check the previous exercise!
-var movie = "The Matrix";
+// var movie = "The Matrix";
 
 // YOUR CODE HERE ------------------------
 
-
+function tellFavouriteMovie() {
+  var movie = "The Matrix";
+  console.log(movie);
+};
 
 // ---------------------------------------
 
 tellFavouriteMovie(); // The Matrix
 movie = "Primer"; // (has no effect on next method call)
 tellFavouriteMovie(); // The Matrix
+
+// Observation: 
+// The function still returns 'The Matrix', but if I call the 'movie' variable,
+// I get Primer returned to me. Two variables named 'movie' but they have no
+// awareness of one another / there's no "same names" conflict.
